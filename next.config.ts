08c40a004bce5f1161092fa04b3e27253577017b
andreslaguilavo <alaguilavo@poligran.edu.co>
2025-11-26 21:05:54 -0500

    Agregar configuración de Next.js con opciones de ESLint, TypeScript, imágenes y variables de entorno

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      }
+    ]
+  },
+  env: {
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
+  }
+}
 
-export default nextConfig;
+export default nextConfig
